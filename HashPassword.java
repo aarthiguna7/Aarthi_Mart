b6aa410f76643372d49d41a9be8cfffb0bdffa42
Aarthi Guna <aarthigunasekaran2007@gmail.com>
2026-09-20 17:20:01 +0530

    chore: add development helper utilities

---
+++ HashPassword.java
@@ -0,0 +1,9 @@
+import org.mindrot.jbcrypt.BCrypt;
+
+public class HashPassword {
+    public static void main(String[] args) {
+        String password = "admin123";
+        String hash = BCrypt.hashpw(password, BCrypt.gensalt());
+        System.out.println(hash);
+    }
+}
